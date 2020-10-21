56230feff43821fe5385a74fb7baabbe4ce5c820
Floris <florismeccanici@gmail.com>
2020-10-21 17:30:35 +0200

    ?

--- rectangle_packing/gui.py
+++ rectangle_packing/gui.py
@@ -343,13 +343,11 @@ class RectanglePackingGui(QWidget):
 
             except ValueError:
                 width = row['Breedte']
-
                 print("Width string has comma")
                 behind_comma = width.split(',')[0]
                 after_comma = width.split(',')[1]
-                
                 width = int(behind_comma)
-            
+                
             try:
                 height = int(row['Lengte'])
 
@@ -361,7 +359,6 @@ class RectanglePackingGui(QWidget):
                 behind_comma = height.split(',')[0]
                 after_comma = height.split(',')[1]
                 height = int(behind_comma)
-
 
             name = row['Ordernummer']
 
