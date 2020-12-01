d7e2e948ac5d5b3990cef4b2830687eade66ecf2
unknown <florismeccanici@gmail.com>
2020-12-01 16:56:51 +0000

    extended vector2d class with new functions

--- rectangle_packing/vector2d.py
+++ rectangle_packing/vector2d.py
@@ -1,3 +1,4 @@
+import math
 
 class Vector2d(object):
     def __init__(self, x, y):
@@ -9,4 +10,19 @@ class Vector2d(object):
     
     def getY(self):
         return self.y
-        
+    
+    @classmethod
+    def fromNumpyArray(cls, array):
+        return Vector2d(array[0], array[1])
+
+    def __subtract__(self, other):
+        return Vector2d((self.getX() - other.getX()),  (self.getY() - other.getY()))
+    
+    def __add__(self, other):
+        return Vector2d((self.getX() + other.getX()),  (self.getY() + other.getY()))
+
+    def __mul__(self, other):
+        return Vector2d((self.getX() * other.getX()),  (self.getY() * other.getY()))
+
+    def norm(self, other):
+        return math.sqrt(self.getX()**2 + self.getY()**2)
